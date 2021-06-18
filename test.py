5311c36d98ce7e92d3a4d749d95cd0de2ead390d
siana <sylvain@siana-systems.com>
2021-06-17 17:46:16 -0700

    first commit of simple test script

---
+++ test.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+from imutils.video import ImageOutput
+
+h_out = ImageOutput(screen=False) # screen=False implies HTTP output
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+
+    h_out.stream("Webcam", frame)
+    time.sleep(1.0/30)
