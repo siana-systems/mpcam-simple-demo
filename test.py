b2f12d8d9c22d14aa70a60e32f36dc0619d3a000
siana <sylvain@siana-systems.com>
2021-06-18 14:27:50 -0700

    add consol message on start

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+print(">> start webcam server...")
 while True:
     ret, frame = cap.read()
     if not ret:
