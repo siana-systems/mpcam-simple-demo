36bc328bc143f2f08d55e811b04e7050f85b21b3
siana <sylvain@siana-systems.com>
2021-07-02 08:48:02 -0700

    added try/finally to release camera + camera configuration

--- test.py
+++ test.py
@@ -4,16 +4,29 @@ from imutils.video import ImageOutput
 
 h_out = ImageOutput(screen=False) # screen=False implies HTTP output
 
+
 cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
-print(">> start webcam server...")
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+try:
+    print(">> starting webcam server on port 8080...")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
 
-    h_out.stream("Webcam", frame)
-    time.sleep(1.0/30)
+        # set channels to zero...
+        #frame[:, :, 0] = 0   # blue
+        #frame[:, :, 1] = 0   # green
+        #frame[:, :, 2] = 0   # red
+
+        h_out.stream("Webcam", frame)
+        time.sleep(1.0/30)
+finally:
+    print(">> releasing camera...")
+    cap.release()
+    print("> done!")
+
